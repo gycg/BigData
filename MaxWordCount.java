8b8430d08ea14ca1d9a21369e940bde5fbff21ec
gycg <1765479220@qq.com>
2016-09-17 00:36:33 +0800

    Task1&2

---
+++ MaxWordCount.java
@@ -0,0 +1,126 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class MaxWordCount {
+
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        context.write(word, one);
+      }
+    }
+  }
+
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+  public static class IdMapper
+       extends Mapper<Object, Text, Text, Text>{
+
+    private Text word_freq = new Text();
+    private Text k    = new Text("-");  //Dummy Key: ensure that all entries go to the same reducer
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      // Complete your 2nd mapper
+        Scanner san = new Scanner(value.toString());
+        while (san.hasNextLine()) {
+          word_freq.set(san.nextLine());
+          context.write(k, word_freq);
+        }
+    }
+  }
+
+  public static class IntMaxReducer
+       extends Reducer<Text,Text,Text,Text> {
+    private Text result = new Text();
+    private String max_word;
+
+    public void reduce(Text key, Iterable<Text> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      // Complete your 2nd reducer
+        int maxVal = -1;
+        Text test = new Text();
+        for (Text val : values) {
+            String valStr = val.toString();
+            String[] tokens = valStr.split("\\t");
+            if(Integer.parseInt(tokens[1]) > maxVal){
+                System.out.println(Integer.parseInt(tokens[1])+"\n");
+                test.set(valStr);
+                maxVal = Integer.parseInt(tokens[1]);
+            }
+        }
+        result.set(test);
+        context.write(key, result);
+    }
+  }
+
+
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(MaxWordCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    job.waitForCompletion(true);
+
+
+
+    Configuration conf2 = new Configuration();
+    Job job2 = Job.getInstance(conf2, "word count");
+    job2.setJarByClass(MaxWordCount.class);
+    job2.setMapperClass(IdMapper.class);
+    job2.setCombinerClass(IntMaxReducer.class);
+    job2.setReducerClass(IntMaxReducer.class);
+    job2.setOutputKeyClass(Text.class);
+    job2.setOutputValueClass(Text.class);
+
+    FileInputFormat.addInputPath(job2, new Path(args[1]));
+    FileOutputFormat.setOutputPath(job2, new Path(args[2]));
+    System.exit(job2.waitForCompletion(true) ? 0 : 1);
+
+
+
+
+  }
+}
